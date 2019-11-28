7aecdd53de45177ba926ffc65f29f2f9d043f4c7
Ribodou <luc.robidou@gmail.com>
2019-11-28 20:34:07 +0900

    remove doc (wich is now on a Google doc)

--- src_backend/src/main.py
+++ src_backend/src/main.py
@@ -6,9 +6,3 @@ import bottles_functions
 if __name__ == "__main__":
     run(bottles_functions.app, host="0.0.0.0", port=8081, quiet=True)
 
-
-# ai_module            | AIs classes
-# bottles_functions    | 
-# errors
-# game_logic_functions
-# main
