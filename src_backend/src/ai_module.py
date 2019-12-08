b07bdc20a3d9112be43629406b45568aae1e2fd4
Ribodou <luc.robidou@gmail.com>
2019-12-08 10:18:40 +0900

    add min-max algorithm and choose it by default

--- src_backend/src/ai_module.py
+++ src_backend/src/ai_module.py
@@ -1,6 +1,7 @@
 from errors import aiCantMoveError
 from game_logic_functions import is_move_valid
-
+import alpha_zero
+import min_max
 
 class AI:
     def __init__(self):
@@ -9,7 +10,15 @@ class AI:
     def __str__(self):
         return repr(self)
     
-    def play(self, inJeu,color):
+    def play(self, inJeu, color, difficulty="normal"):
+        if difficulty == "easy":
+            return self.play_random(inJeu, color)
+        elif difficulty == "normal":
+            return self.play_minmax(inJeu, color)
+        elif difficulty == "hard":
+            return self.play_alpha_zero(inJeu, color)
+    
+    def play_random(self, inJeu,color):
         """
             Play for ai or raise aiCantMoveError if the ai con't play.
         """
@@ -20,3 +29,15 @@ class AI:
                     print("I, the ai, play", j, i)
                     return inJeu, j, i
         raise aiCantMoveError("the ai can't move")
+    
+    def play_minmax(self, inJeu,color):
+        game,row,col = min_max.play_ia(inJeu)
+        if(row != None):
+            print("I, the ai, play", row, col)
+            return game, row, col
+        print("What", game)
+        raise aiCantMoveError("the ai can't move")
+        print("Whut")
+    
+    def play_alpha_zero(self, inJeu, color):
+        pass  # TODO
